9a089bb3d5425c23b18370752be993f425fe55b2
GabrielDev2458 <gabriel_hm24@outlook.com>
2021-11-14 23:55:44 -0600

    add basic lessons

---
+++ lesson1/basics.js
@@ -0,0 +1,23 @@
+//--- alerts
+alert('this is an alert');
+
+//--- Prompt()
+let userName = prompt("enter your name");
+console.log(userName);
+
+//--- Logical operators
+let result = true && true;
+console.log(result);
+
+result = true || false;
+console.log(result);
+
+result = true || false || false;
+console.log(result);
+
+//--- ternary operator
+let flag = true;
+
+let message = flag ? 'the value of the flag is: true' : 'the value of the flag is: false';
+
+console.log(message);
