297c77972705831ec08390db8f0c4bb1e381dd6a
gabriel.hernandez.mejia.contractor <gabriel.hernandez.mejia.contractor@bbva.com>
2021-11-16 16:22:05 -0600

    add theory about fragment and templates

--- DOM/app.js
+++ DOM/app.js
@@ -26,8 +26,61 @@ boton.addEventListener("click", () => {
     parrafo.textContent = "Nuevo texto desde evento";
 });
 
-//createElement
+//createElement and appendChild
+const listaDinamica = document.querySelector("#listaDinamica");
 
 const li = document.createElement("li");
 li.textContent = "item desde javascript";
-console.log(li);
+
+listaDinamica.appendChild(li);
+
+/**
+ * Fragment
+ * 
+ * TODO: translate to english.
+ * 
+ * *new DocumentFragment()
+ * *createDocumentFragment()
+ * 
+ * *La interfaz DocumentFragment representa un objeto de documento mínimo que no tiene padre.
+ * *Se utiliza como una versión ligera de Document que almacena un segmento de una estructura de documento compuesta de nodos como un documento estándar.
+ * *La gran diferencia se debe al hecho de que el fragmento de documento no forma parte de la estructura de árbol del documento activo.
+ * *Los cambios realizados en el fragmento no afectan el documento (incluso en reflow) ni inciden en el rendimiento cuando se realizan cambios.
+*/
+
+const countries = ["Perú", "Bolivia", "Colombia"];
+
+const fragment = document.createDocumentFragment();
+
+countries.forEach((country) => {
+    const li = document.createElement("li");
+    li.textContent = country;
+    fragment.appendChild(li);
+});
+
+listaDinamica.appendChild(fragment);
+
+/**
+ * Template
+ * 
+ * TODO: translate to english.
+ * 
+ * *Es un mecanismo para mantener el contenido HTML del lado del cliente que no se renderiza cuando se carga una página, pero que posteriormente puede ser instanciado durante el tiempo de ejecución empleando JavaScript.
+ * *Piensa en la plantilla como un fragmento de contenido que está siendo almacenado para un uso posterior en el documento.
+ * *El analizador procesa el contenido del elemento <template> durante la carga de la página, pero sólo lo hace para asegurar que esos contenidos son válidos; sin embargo, estos contenidos del elemento no se renderizan.
+ * *Los elementos <template> contienen un DocumentFragment en su propiedad HTMLTemplateElement.content.
+*/
+
+const secondFragment = document.createDocumentFragment();
+const liTemplate = document.querySelector("#liTemplate");
+const clickCountry = (e) => console.log("evento:", e.target);
+
+countries.forEach((country) => {
+    const clone = liTemplate.content.firstElementChild.cloneNode(true);
+    clone.querySelector("span").textContent = country;
+    clone.addEventListener("click", clickCountry);
+
+    secondFragment.appendChild(clone);
+});
+
+listaDinamica.appendChild(secondFragment);
