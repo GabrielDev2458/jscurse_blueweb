b0bcd0470d7e967d68a7fe383cc189e00a22a794
GabrielDev2458 <gabriel_hm24@outlook.com>
2021-11-16 21:35:11 -0600

    add more array methods 'some' 'findIndex' 'slice'

--- arrayMethods/app.js
+++ arrayMethods/app.js
@@ -19,3 +19,26 @@ console.log(higher);
 //find
 const amy = users.find((user) => user.uid === 2);
 console.log(amy);
+
+//some
+const exist = users.some((user) => user.uid === 2);
+console.log(exist);
+
+/**
+ * findIndex
+ * TODO: Translate.
+ **El método findIndex() devuelve el índice del primer elemento de un array que cumpla con la función de prueba proporcionada. En caso contrario devuelve -1
+*/
+const indexFound = users.findIndex((user) => user.uid === 4);
+console.log(indexFound);
+
+//Slice
+const arr = ["Cat", "Dog", "Tiger", "Zebra"];
+//             0      1       2        3
+const newArray = arr.slice(1, 3);
+
+console.log(newArray);
+
+/**
+ * TODO: REDUCE and spread min 1:14
+ */
